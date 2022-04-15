44c93d4bda43c2f5af96255becfdcb7aa76e04b9
ozkanpakdil <ozkan.pakdil@gmail.com>
2022-04-15 21:08:35 +0100

    quarkus upgrade and dockerize the project

--- src/main/java/com/mascix/CommandService.java
+++ src/main/java/com/mascix/CommandService.java
@@ -1,24 +1,22 @@
 package com.mascix;
 
+import io.quarkus.cache.CacheResult;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 @ApplicationScoped
 public class CommandService {
-
     private final Logger log = LoggerFactory.getLogger(CommandService.class);
 
-    @Inject
-    CacheHelper cache;
-
-    public String execToString(String ip) throws IOException, InterruptedException {
+    @CacheResult(cacheName = "cmd-cache")
+    public String execToString(String ip) {
         String c1 = "/bin/sh";
         String[] cmd = {c1, "-c", "torify whois " + ip};
         if (System.getProperty("os.name").toLowerCase().contains("windows"))// check is OS windows
@@ -27,18 +25,19 @@ public class CommandService {
             cmd = new String[]{c1, "/c", "dir"};
         }
 
-        if (cache.get(ip) != null) {
-            return (String) cache.get(ip);
+        log.info("cmd:{}", Arrays.toString(cmd));
+
+        String result = "";
+        try {
+            ProcessBuilder pb = new ProcessBuilder(cmd);
+            Process p = pb.start();
+            p.waitFor(60, TimeUnit.SECONDS);
+
+            result = IOUtils.toString(p.getInputStream(), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            log.error("runcmd error:", e);
         }
 
-        log.info("ip:{}", ip);
-        String result;
-        ProcessBuilder pb = new ProcessBuilder(cmd);
-        Process p = pb.start();
-        p.waitFor(60, TimeUnit.SECONDS);
-
-        result = IOUtils.toString(p.getInputStream(), StandardCharsets.UTF_8);
-        cache.put(ip, result);
         return result;
     }
 }
